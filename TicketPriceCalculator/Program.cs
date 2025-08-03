075728b639776c00e8e3ebbab43677fd6a2827b2
peter-kwame <pkaforve@st.ug.edu.gh>
2025-08-03 12:51:45 +0000

    Add Ticket Price Calculator application

---
+++ TicketPriceCalculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter your age: ");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        int price;
+
+        if (age <= 12 || age >= 65)
+        {
+            price = 7;
+        }
+        else
+        {
+            price = 10;
+        }
+
+        Console.WriteLine("Your ticket price is GHC" + price);
+    }
+}
